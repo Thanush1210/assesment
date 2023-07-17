febe520993e6fd2fff862b0ab605bc97c8b0f7e1
Thanush1210 <132657692+Thanush1210@users.noreply.github.com>
2023-07-17 10:35:08 +0530

    After debugging errors

--- social-media/src/views/DisplayView.vue
+++ social-media/src/views/DisplayView.vue
@@ -75,14 +75,6 @@ export default {
     UsersData,
   },
   methods: {
-    convertToBase64(file) {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = (error) => reject(error);
-        reader.readAsDataURL(file);
-      });
-    },
     addData(data) {
       const post = {
         image: data.image,
